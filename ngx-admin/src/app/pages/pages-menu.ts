ab9db7cfdba1e94cf998641a68f57f8944b2140e
Vanessa Nájera <vjndovale@gmail.com>
2022-11-27 11:57:59 -0500

    changes dashboard

--- ngx-admin/src/app/pages/pages-menu.ts
+++ ngx-admin/src/app/pages/pages-menu.ts
@@ -3,7 +3,7 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Inicio',
-    icon: 'shopping-cart-outline',
+    icon: 'home',
     link: '/pages',
     home: true,
   },
@@ -18,7 +18,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
       },
       {
         title: 'Reset Password',
-        link: '/pages/',
+        link: '/pages/users/crear',
       },
     ],
   },
